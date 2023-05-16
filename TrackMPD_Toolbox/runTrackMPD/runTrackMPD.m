a2db3fde8ed694411704a9d1fc2ee1727de317b3
IJalonRojas <ijalonrojas@gmail.com>
2023-05-16 10:30:25 +0200

    Update runTrackMPD.m
    Temporary change until verification

--- TrackMPD_Toolbox/runTrackMPD/runTrackMPD.m
+++ TrackMPD_Toolbox/runTrackMPD/runTrackMPD.m
@@ -626,25 +626,12 @@ for k=1:numpartition % EXTERNAL LOOP
       
       % Optimization of the interpolations by reducing the data matrices in the temporal dimension
       [~,Ind1] = min(abs(TT-tspan(1)));
-      disp(['Ind1 : ' num2str(Ind1)])
-      if direction > 1
-        if TT(Ind1)>tspan(1)
-          Ind1=Ind1-1;
-        end
-      else
-        if TT(Ind1)<tspan(1)
-          Ind1=Ind1-1;
-        end  
+      if TT(Ind1)>tspan(1)
+        Ind1=Ind1-1;
       end
       [~,Ind2] = min(abs(TT-tspan(end)));
-      if direction > 1
-        if TT(Ind2)<tspan(end)
-          Ind2=Ind2+1;
-        end
-      else
-        if TT(Ind2)>tspan(end)
-          Ind2=Ind2+1;
-        end  
+      if TT(Ind2)<tspan(end)
+        Ind2=Ind2+1;
       end
       Ind = Ind1:Ind2;
       TTs = TT(Ind1:Ind2);  
