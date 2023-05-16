d0d1ee0c00ce35d8ca35ecf1b674162a531d19dc
vmarieu <vincent.marieu@u-bordeaux.fr>
2023-05-16 14:29:37 +0200

    Update runTrackMPD.m
    Corrected bug on tspan optimization

--- TrackMPD_Toolbox/runTrackMPD/runTrackMPD.m
+++ TrackMPD_Toolbox/runTrackMPD/runTrackMPD.m
@@ -625,16 +625,36 @@ for k=1:numpartition % EXTERNAL LOOP
       tspan=tspan_chunk(i)-direction*TimeStepOut:direction*TimeStepCalc:tspan_chunk(i);
       
       % Optimization of the interpolations by reducing the data matrices in the temporal dimension
+      %   disp('tspan : ')
+      %   disp(num2str(tspan))
+      %   disp('TT : ')
+      %   disp(num2str(TT))
       [~,Ind1] = min(abs(TT-tspan(1)));
-      if TT(Ind1)>tspan(1)
-        Ind1=Ind1-1;
+      %   disp(['Ind1 : ' num2str(Ind1)])
+      if direction > 0
+        if TT(Ind1(1))>tspan(1)
+          Ind1=Ind1-1;
+        end
+      else
+        if TT(Ind1(1))<tspan(1)
+          Ind1=Ind1-1;
+        end  
       end
+      %   disp(['Ind1 : ' num2str(Ind1)])
       [~,Ind2] = min(abs(TT-tspan(end)));
-      if TT(Ind2)<tspan(end)
-        Ind2=Ind2+1;
+      %   disp(['Ind2 : ' num2str(Ind2)])
+      if direction > 0
+        if TT(Ind2(1))<tspan(end)
+          Ind2=Ind2+1;
+        end
+      else
+        if TT(Ind2(1))>tspan(end)
+          Ind2=Ind2+1;
+        end  
       end
-      Ind = Ind1:Ind2;
-      TTs = TT(Ind1:Ind2);  
+      %   disp(['Ind2 : ' num2str(Ind2)])
+      Ind = Ind1(1):Ind2(1);
+      TTs = TT(Ind);  
       Us = U(:,:,:,Ind);
       Vs = V(:,:,:,Ind);
       Ws = W(:,:,:,Ind);
